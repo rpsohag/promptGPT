91bf902c5dcd7fef50664ac8bbd91acb42f084f9
RP SOHAG <rpsohag.bd@gmail.com>
2023-08-20 22:32:29 +0600

    fix problems

--- utils/database.js
+++ utils/database.js
@@ -3,13 +3,15 @@ import mongoose from "mongoose";
 let isConnected = false;
 
 export const connectToDB = async () => {
-  mongoose.set("strict", true); // Corrected key: "strict" instead of "strictQuery"
+  mongoose.set("strict", true);
   if (isConnected) {
     console.log("MongoDB is already connected");
     return;
   }
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "promptGPT",
+    });
     isConnected = true;
     console.log("MongoDB Connected");
   } catch (error) {
